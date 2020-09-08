0537e8b168d1001d562b767aef59a6782916667d
Eileen Petros <emp110@case.edu>
2020-09-08 14:10:44 -0500

    URPSetup
    Set up project to use 2D lights.

---
+++ EscapeForest/Assets/Scripts/BasePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePlayer : MonoBehaviour
+{
+
+    private enum element { None,Air,Earth,Fire,Water};
+    private element currenElement = element.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currenElement = (element)Random.Range(0, 4);
+            Debug.Log(currenElement);
+        }
+    }
+}
