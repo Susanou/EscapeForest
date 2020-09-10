ac619e43d54cffd595ea5b7aec1b33fd8eeb14e5
Fukurou <caos2804@gmail.com>
2020-09-10 21:24:03 +0200

    NPC Skeleton

---
+++ EscapeForest/Assets/Scripts/NPCSkeleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSkeleton : MonoBehaviour
+{
+
+    public string hint = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.Space)){
+            Debug.Log(hint);
+        }
+        
+    }
+}
