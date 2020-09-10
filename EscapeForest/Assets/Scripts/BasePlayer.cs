423be284da6bb06d059769e30367e604698ca7ff
Eileen Petros <emp110@case.edu>
2020-09-10 13:36:39 -0500

    Sanity and Simple Player Movement (WASD)
    Sanity meter in BasePlayer code

--- EscapeForest/Assets/Scripts/BasePlayer.cs
+++ EscapeForest/Assets/Scripts/BasePlayer.cs
@@ -10,13 +10,40 @@ public class BasePlayer : MonoBehaviour
      * @author 
      * 
      */
+    [SerializeField] private Transform playerTransform;
 
+    private float speed = 25f;
     public enum element { None,Air,Earth,Fire,Water};
     private element currentElement = element.None;
+
+    //TODO Sanity
+    private int sanityLevel = 25;
 
     public element getCurrentElement()
     {
         return currentElement;
+    }
+
+    public int getSanity()
+    {
+        return sanityLevel;
+    }
+
+    public void increaseSanity(int amount)
+    {
+        sanityLevel += amount;
+    }
+
+    public void decreaseSanity(int amount)
+    {
+        if (sanityLevel -amount < 0)
+        {
+            sanityLevel = 0;
+        }
+        else
+        {
+            sanityLevel -= amount;
+        }
     }
 
 
@@ -29,10 +56,44 @@ public class BasePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerMovement();
         if (Input.GetKeyDown(KeyCode.E))
         {
             currentElement = (element)Random.Range(0, 5);
             Debug.Log(currentElement);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            increaseSanity(5);
+            Debug.Log(sanityLevel);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            decreaseSanity(10);
+            Debug.Log(sanityLevel);
+        }
+    }
+
+    private void playerMovement()
+    {
+        Vector2 pos = playerTransform.position;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+        {
+            playerTransform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerTransform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerTransform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerTransform.position += Vector3.right * speed * Time.deltaTime;
         }
     }
 }
