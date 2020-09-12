62dbf8f80c16db7b9a0fb791882aa9b119dc814a
Fukurou <caos2804@gmail.com>
2020-09-12 11:59:11 +0200

    Sanity Scriptable object

---
+++ EscapeForest/Assets/Scripts/SignalListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SignalListener : MonoBehaviour
+{
+    public Signal signal;
+    public UnityEvent signalEvent;
+
+    public void OnSignalRaised()
+    {
+        signalEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        signal.registerListener(this);
+    }
+
+    private void OnDisable()
+    {
+        signal.deregisterLinstener(this);
+
+    }
+}
