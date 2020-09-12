dbf4a81f6d3ab8b78aeaee5b244d8fe62f2f749b
James Hwang <siro257@gmail.com>
2020-09-11 18:14:22 -0700

    added InteractionObject and comment

---
+++ EscapeForest/Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    // James
+    public void DoInteraction()
+    {
+        // Todo: pick up the obejct, put it in the inventory
+
+        // disable the object
+        gameObject.SetActive(false);
+    }
+}
