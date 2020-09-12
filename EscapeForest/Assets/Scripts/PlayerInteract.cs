f666e2ab500f4c41c9e1ab45a300eb916777d9b9
James Hwang <siro257@gmail.com>
2020-09-11 18:05:28 -0700

    PlayerInteract

---
+++ EscapeForest/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+
+
+    public GameObject currentInterObj = null;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && currentInterObj)
+        {
+            // pick up the object? 
+            Debug.Log("Pickup");
+            
+        }
+    }
+
+    // Detects interactble objects by looking at a tag.
+    // When the player gets in the range of the object's collider, 
+    // this code will return(log) the name of the interactable object and sets the current object as a current interactable object.
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("interactable"))
+        {
+            Debug.Log(collision.name);
+            currentInterObj = collision.gameObject;
+        }
+
+    }
+
+    // if the player gets out of the range of the object's collider, the current interactable object sets to null.
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("interactable"))
+        {
+            currentInterObj = null;
+        }
+    }
+}
