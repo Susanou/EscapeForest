e57d7e4c9bb5549f9fff7f13de56d2cbd40e4145
Eileen Petros <emp110@case.edu>
2020-09-15 19:48:19 -0500

    Interactable Decreases Sanity by Different amounts

--- EscapeForest/Assets/DecreaseSanityOnCollide.cs
+++ EscapeForest/Assets/DecreaseSanityOnCollide.cs
@@ -6,15 +6,30 @@ public class DecreaseSanityOnCollide : MonoBehaviour
 {
 
     private BasePlayer playerScript;
+
+    //Set the costs (in inspector) of interacting with this object for different elements
+    [SerializeField] private int[] setSanityCosts = new int[4]; //TODO change names of array elements in inspector
+
+    Dictionary<BasePlayer.element, int> sanityCosts = new Dictionary<BasePlayer.element, int>(); //none air earth fire water
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        sanityCosts.Add(BasePlayer.element.None, 0);
+        sanityCosts.Add(BasePlayer.element.Air, setSanityCosts[1]);
+        sanityCosts.Add(BasePlayer.element.Earth, setSanityCosts[2]);
+        sanityCosts.Add(BasePlayer.element.Fire, setSanityCosts[3]);
+        sanityCosts.Add(BasePlayer.element.Water, setSanityCosts[4]);
+
         playerScript = GameObject.Find("Player").GetComponent<BasePlayer>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        playerScript.decreaseSanity(5);
+        Debug.Log(sanityCosts[playerScript.getCurrentElement()]);
+        playerScript.decreaseSanity(sanityCosts[playerScript.getCurrentElement()]);
     }
 
 
