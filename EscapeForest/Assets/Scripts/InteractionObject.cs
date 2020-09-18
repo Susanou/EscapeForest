ff72e5ff7924c554f0104f2e80a08dcab2113d76
Michael Cunningham <michaelbcunningham51@gmail.com>
2020-09-18 11:51:21 -0400

    Added interactable, prints which element if tree clicked, disappears if fire.

--- EscapeForest/Assets/Scripts/InteractionObject.cs
+++ EscapeForest/Assets/Scripts/InteractionObject.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class InteractionObject : MonoBehaviour
 {
+    private BasePlayer player;
+    private void Start() {
+        player = BasePlayer.instance;
+    }
+    private void OnMouseDown() {
+        DoInteraction();
+    }
     // James
     public void DoInteraction()
     {
-        // Todo: Detect if it's a pickupable item or not. (pickup items vs. plant, torch, etc. )
+        if (player.getCurrentElement() == BasePlayer.element.Air) {
+            Debug.Log("Air interaction");
+        }
+        else if (player.getCurrentElement() == BasePlayer.element.Water) {
+            Debug.Log("Water interaction");
+        }
+        else if (player.getCurrentElement() == BasePlayer.element.Fire) {
+            Debug.Log("Fire interaction");
+            gameObject.SetActive(false);
+        }
+        else if (player.getCurrentElement() == BasePlayer.element.Earth) {
+            Debug.Log("Earth interaction");
+        }
 
-        // if picupable, put it in the inventory & disable the object.
-
-        // Todo: implement inventory 
-        gameObject.SetActive(false);
     }
 }
