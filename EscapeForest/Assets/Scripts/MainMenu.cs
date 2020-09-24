6aecaba1bac6b646b9cf8be70077bb2331e7f4c0
Ally Dinhofer <ally.dinhofer@gmail.com>
2020-09-24 16:34:08 -0400

    Made menu and instructions screen
    Created the menu and instructions pages. Set up to attach to game

---
+++ EscapeForest/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Test");
+    }
+
+    public void ShowInstructions()
+    {
+        SceneManager.LoadScene("Instructions");  
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
