759c973a10f518ec9163b6eff17073d228d172ad
Fukurou <caos2804@gmail.com>
2020-09-26 14:33:22 +0200

    Abstract Element class $16q

---
+++ EscapeForest/Assets/Scripts/Element.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Element : MonoBehaviour
+{
+    
+    public int[] sanityCosts = new int[8]; // Order should be the same as on `Sanity meter Specifics` document in Drive
+
+    public virtual void OnLeftClick(){
+        if(Input.GetMouseButtonDown(0)){
+            Debug.Log("LeftClick pressed");
+        }
+    }
+
+    public virtual void OnLeftClickDrag(){
+        if(Input.GetMouseButton(0)){
+            Debug.Log("LeftClick dragging");
+        }
+    }
+
+    public virtual void OnRightClick(){
+        if(Input.GetMouseButtonDown(1)){
+            Debug.Log("RightClick pressed");
+        }
+    }
+
+    public virtual void OnRightClickDrag(){
+        if(Input.GetMouseButton(1)){
+            Debug.Log("RightClick dragging");
+        }
+    }
+
+}
