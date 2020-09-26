9a20298c9a8f2a4f324ff53a022a0e17096085d1
Fukurou <caos2804@gmail.com>
2020-09-26 18:06:13 +0200

    Projectile script $16v

---
+++ EscapeForest/Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float speed = 10.0f;
+    [SerializeField] private float lifespan;
+
+    private float dirx;
+    private float diry;
+
+    public Projectile(float x, float y){
+        this.dirx = x;
+        this.diry = y;
+    }
+
+	void Update() {
+		transform.Translate(dirx*speed*Time.deltaTime, diry*speed*Time.deltaTime, 0);
+
+        this.lifespan -= Time.deltaTime;
+
+        if(this.lifespan < 0){
+            Destroy(this.gameObject);
+        }
+	}
+}
