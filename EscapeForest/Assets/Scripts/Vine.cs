a354a91fcca6f30402ffd66b3788799445dd5faa
Michael Cunningham <mbc95@case.edu>
2020-10-03 11:55:37 -0400

    added vine interactions with lilypads

---
+++ EscapeForest/Assets/Scripts/Vine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vine : MonoBehaviour
+{
+    private BasePlayer player;
+    [SerializeField] private GameObject lily;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = BasePlayer.instance;
+        lily.SetActive(false);
+    }
+    void OnMouseUp() // mouse click
+{
+        if (player.getCurrentElement() == BasePlayer.element.Water)
+            lily.SetActive(true);
+    }
+
+    void OnTriggerEnter() // for trigger
+    {
+        if (player.getCurrentElement() == BasePlayer.element.Water)
+            lily.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
