40b4a8a8d139ceeaaeed412d1c9886cffd5e048f
Fukurou <caos2804@gmail.com>
2020-10-07 22:16:55 +0200

    Zip build version

--- EscapeForest/Assets/Scripts/level2goal.cs
+++ EscapeForest/Assets/Scripts/level2goal.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class level2goal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        SceneManager.LoadScene("Level3");
     }
 }
