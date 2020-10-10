cba3c7e7568ec9f05d8c840146302ab2934f588d
Talia Eshenbaugh <taliaeshenbaugh@gmail.com>
2020-10-10 14:47:53 -0400

    Tutorial Level Complete
    Finished Tutorial Level

--- EscapeForest/Assets/Scripts/Character/BasePlayer.cs
+++ EscapeForest/Assets/Scripts/Character/BasePlayer.cs
@@ -42,17 +42,24 @@ public class BasePlayer : MonoBehaviour
     private bool randomMovementEnabled = false;
     private bool randomElementEnabled = false;
 
+    private bool airEnabled = true;
+    private bool earthEnabled = true;
+    private bool fireEnabled = true;
+    private bool waterEnabled = true;
+
     //Element variables
 
     //private element currentElement = element.None;
 
     private KeyCode[] inputKeyCodes = new[] { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
 
-    [SerializeField] private Element[] elementsArray = new Element[4];
+    [SerializeField] private Element[] elementArray = new Element[4];
+    private Element[] elementsArray = new Element[4];
     private Element usingElement;
 
     private void Start()
     {
+        elementsArray = elementArray;
         if(particle.isPlaying) particle.Stop();
         sanityBar.setSanity(this.getSanity());
     }
@@ -94,7 +101,7 @@ public class BasePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && waterEnabled)
         {
             currentElement.RuntimeValue = (element)Random.Range(0, 5);
             usingElement = elementsArray[(int)currentElement.RuntimeValue];
@@ -102,28 +109,28 @@ public class BasePlayer : MonoBehaviour
         }
 
  
-        if (Input.GetKeyDown(inputKeyCodes[0])) // Air = 1
+        if (Input.GetKeyDown(inputKeyCodes[0]) && airEnabled) // Air = 1
         {
             currentElement.RuntimeValue = element.Air;
             usingElement = elementsArray[0];
             elementChanged.Raise();
 
         }
-        else if (Input.GetKeyDown(inputKeyCodes[1])) // Earth = 2
+        else if (Input.GetKeyDown(inputKeyCodes[1]) && earthEnabled) // Earth = 2
         {
             currentElement.RuntimeValue = element.Earth;
             usingElement = elementsArray[1];
             elementChanged.Raise();
 
         }
-        else if (Input.GetKeyDown(inputKeyCodes[2])) // Fire = 3
+        else if (Input.GetKeyDown(inputKeyCodes[2]) && fireEnabled) // Fire = 3
         {
             currentElement.RuntimeValue = element.Fire;
             usingElement = elementsArray[2];
             elementChanged.Raise();
 
         }
-        else if (Input.GetKeyDown(inputKeyCodes[3])) // Water = 4
+        else if (Input.GetKeyDown(inputKeyCodes[3]) && waterEnabled) // Water = 4
         {
             currentElement.RuntimeValue = element.Water;
             usingElement = elementsArray[3];
@@ -227,5 +234,40 @@ public class BasePlayer : MonoBehaviour
                 inputKeyCodes[i] = temp;
             }
         }
+    }
+
+
+
+    public void TutorialMode(int step)
+    {
+        if(step == 0)
+        {
+            //elementsArray = new Element[] {element.None, element.None, element.None, element.None};
+            airEnabled = false;
+            earthEnabled = false;
+            fireEnabled = false;
+            waterEnabled = false;
+        }
+        else if(step == 1)
+        {
+            // elementsArray = new Element[] {elementArray[0], element.None, element.None, element.None};
+            airEnabled = true;
+        }
+        else if(step == 2)
+        {
+            // elementsArray = new Element[] {elementArray[0], elementArray[1], element.None, element.None};
+            earthEnabled = true;
+        }
+        else if(step == 3)
+        {
+            //elementsArray = new Element[] {elementArray[0], elementArray[1], elementArray[2], element.None};
+            fireEnabled = true;
+        }
+        else if(step == 4)
+        {
+            // elementsArray = elementArray;
+            waterEnabled = true;
+        }
+
     }
 }
