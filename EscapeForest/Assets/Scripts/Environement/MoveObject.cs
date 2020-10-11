a3ecd7e983fe86c6a6a837d7c6d96bd2a1c4dbe7
Eileen Petros <emp110@case.edu>
2020-10-10 22:08:59 -0500

    Fix for Platforming
    Level 1 Platform only moves up once

--- EscapeForest/Assets/Scripts/Environement/MoveObject.cs
+++ EscapeForest/Assets/Scripts/Environement/MoveObject.cs
@@ -7,17 +7,23 @@ public class MoveObject : MonoBehaviour
     private Transform transform;
     private int speed = 2;
     private Vector3 initialPos;
+    private bool moved;
     // Start is called before the first frame update
     void Start()
     {
         transform = gameObject.GetComponent<Transform>();
         initialPos = transform.position;
+        moved = false;
     }
 
     public void moveY(int distance)
     {
         //StartCoroutine(moveYOverTime(3));
-        transform.position = transform.position + Vector3.up * distance;
+        if (!moved)
+        {
+            transform.position = transform.position + Vector3.up * distance;
+            moved = true;
+        }
     }
 
     public IEnumerator moveYOverTime(int distance)
