003bb0dd3edc4180122c63f23d00f7d1cbada105
Eileen Petros <emp110@case.edu>
2020-10-12 11:44:24 -0500

    EmptyPit Collider will not Interact with Player
    Remaining bugs: can't rise with platform if already on top of it, reset from spikes not working

---
+++ EscapeForest/Assets/Scripts/IgnorePlayerCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnorePlayerCollider : MonoBehaviour
+{
+
+    private BoxCollider2D playerCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>();
+        Physics2D.IgnoreCollision(playerCollider, gameObject.GetComponent<BoxCollider2D>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
