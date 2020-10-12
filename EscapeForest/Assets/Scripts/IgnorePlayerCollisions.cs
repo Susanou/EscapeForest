cfb6fb484d53025235ec69740cbe0b0eb11b6ff3
Eileen Petros <emp110@case.edu>
2020-10-12 17:00:45 -0500

    Level 1 New Solutions
    Pit of water can be filled and unfilled and frozen and unfrozen

--- EscapeForest/Assets/Scripts/IgnorePlayerCollisions.cs
+++ EscapeForest/Assets/Scripts/IgnorePlayerCollisions.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IgnorePlayerCollider : MonoBehaviour
+public class IgnorePlayerCollisions : MonoBehaviour
 {
 
     private BoxCollider2D playerCollider;
+    private BoxCollider2D pitCollider;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>();
-        Physics2D.IgnoreCollision(playerCollider, gameObject.GetComponent<BoxCollider2D>());
+        pitCollider = gameObject.GetComponent<BoxCollider2D>();
+
+        //Let player fall into pit
+        Physics2D.IgnoreCollision(playerCollider, pitCollider);
+
+
     }
 
     // Update is called once per frame
