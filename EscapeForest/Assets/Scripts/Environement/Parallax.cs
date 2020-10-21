70cf2597dbeb723167e2e9d697337ba8f44a4bd8
Eileen Petros <emp110@case.edu>
2020-10-20 20:10:37 -0500

    Parallax Effect L1 + Orthographic Camera

---
+++ EscapeForest/Assets/Scripts/Environement/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] float multiplier = 0.0f;
+    [SerializeField] bool horizontalOnly = true;
+
+    private Transform cameraTransform;
+
+    private Vector3 startCameraPos;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        startCameraPos = cameraTransform.position;
+        startPos = transform.position;
+    }
+
+
+    private void LateUpdate()
+    {
+        var position = startPos;
+        if (horizontalOnly)
+            position.x += multiplier * (cameraTransform.position.x - startCameraPos.x);
+        else
+            position += multiplier * (cameraTransform.position - startCameraPos);
+
+        transform.position = position;
+    }
+}
