bbad99001a3ebac447251d502a623984e1805525
Ally Dinhofer <ally.dinhofer@gmail.com>
2020-10-24 18:33:51 -0400

    fixed level 3 UI bug

--- EscapeForest/Assets/Scripts/UI/MainMenu.cs
+++ EscapeForest/Assets/Scripts/UI/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Test");
+        SceneManager.LoadScene("Tutorial");
     }
 
     public void LevelSelect()
