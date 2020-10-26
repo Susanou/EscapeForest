9c9724dbe0ea7d4ff9bb990a3a2ab515f837e907
Eileen Petros <emp110@case.edu>
2020-10-26 00:25:27 -0500

    Camera Work

---
+++ EscapeForest/Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    [SerializeField] string activatorTag = null;
+    [SerializeField] bool deactivateOnExit = false;
+    [SerializeField] GameObject[] objects = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision");
+        if (collision.CompareTag(activatorTag))
+        {
+            foreach (var obj in objects)
+                obj.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (deactivateOnExit && collision.CompareTag(activatorTag))
+        {
+            foreach (var obj in objects)
+                obj.SetActive(false);
+        }
+    }
+}
