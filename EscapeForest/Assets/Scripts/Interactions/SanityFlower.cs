c77513ece45d91bc130385c98df056e969a4f4f2
Fukurou <caos2804@gmail.com>
2020-10-29 14:19:15 +0100

    Flower ragain sanity prefab

---
+++ EscapeForest/Assets/Scripts/Interactions/SanityFlower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SanityFlower : MonoBehaviour
+{
+
+    public float sanityGain;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other.gameObject.tag == "Player");
+
+        if(other.gameObject.tag == "Player"){
+            other.gameObject.GetComponent<BasePlayer>().addSanityOf(sanityGain);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
