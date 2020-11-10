e5d2c837dad7c0fb500d15a90fda822ebf80c5dc
Eileen Petros <emp110@case.edu>
2020-11-09 18:23:51 -0600

    L1 Fixes and Tutorial Camera

--- EscapeForest/Assets/Scripts/Camera/CameraMovement.cs
+++ EscapeForest/Assets/Scripts/Camera/CameraMovement.cs
@@ -34,7 +34,7 @@ public class CameraMovement : MonoBehaviour
         switch (currentScene)
         {
             case "Tutorial":
-                yPos = 10f;
+                yPos = -1f;
                 break;
             case "Level1":
                 yPos = -1f;
--- EscapeForest/Assets/Scripts/Environement/Parallax_2.cs
+++ EscapeForest/Assets/Scripts/Environement/Parallax_2.cs
@@ -5,29 +5,48 @@ using UnityEngine;
 public class Parallax_2 : MonoBehaviour
 {
     private float length;
-    private float startPosition;
+    private float initialX;
     public GameObject cam;
     public float parallaxEffect;
-    
+
+    private Vector3 startPos;
+    private float initialY;
+    private float yPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position.x;
+        startPos = transform.position;
+        initialX = transform.position.x;
+        initialY = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    //Shifting camera in tutorial
+    public void changeY(float newY)
+    {
+        startPos.y = startPos.y + newY;
+    }
+
+
+    public void resetY()
+    {
+        startPos.y = initialY;
     }
 
     void FixedUpdate()
     {
+        Debug.Log(startPos.y);
         float temp = (cam.transform.position.x * (1-parallaxEffect));
         float distance = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startPosition + distance, transform.position.y,transform.position.z);
-        if(temp > startPosition + length)
+        transform.position = new Vector3(initialX + distance,startPos.y,transform.position.z);
+        if(temp > initialX + length)
         {
-            startPosition += length;
+            initialX += length;
         }
-        else if (temp < startPosition - length)
+        else if (temp < initialX - length)
         {
-            startPosition -= length;
+            initialX -= length;
         }
     }
 }
